0b9b1d16308641a432be159492a62fd6dc45cb87
Laiot <carmelo.sarta.main@gmail.com>
2022-05-24 18:30:36 +0200

    Getting words length and initialized vocabulary

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 use std::io::*;
 
+fn get_input() -> String{
+    let mut raw_input = String::new();
+    stdin().read_line(&mut raw_input).expect("Something's wrong with your input!");
+    if let Some('\n') = raw_input.chars().next_back() {
+        raw_input.pop();
+    }
+    if let Some('\r') = raw_input.chars().next_back() {
+        raw_input.pop();
+    }
+    return raw_input;
+}
+
+fn create_vocabulary(words_len: usize) -> Vec<String>{
+    let mut vocabulary: Vec<String> = Vec::new();
+
+    loop{
+        let new_word = get_input();
+        if new_word == "new_game"{
+            return vocabulary;
+        }
+        if new_word.len() == words_len && !vocabulary.contains(&new_word){
+            vocabulary.push(new_word);
+        }
+    }
+}
+
 fn main() {
     println!("Welcome to a Rust-y version of Wordle!\nEnter `h` to ask for help.");
-    loop {
-        let _=stdout().flush();
-        let mut raw_input = String::new();
-        stdin().read_line(&mut raw_input).expect("Something's wrong with your input!");
-        if let Some('\n')=raw_input.chars().next_back() {
-            raw_input.pop();
-        }
-        println!("You typed: {}",raw_input);
-    }
+    
+    let words_len: usize = get_input().parse().unwrap();
+
+    let mut vocabulary: Vec<String> = create_vocabulary(words_len);
+
+    println!("{:?}", vocabulary);
+
 }
